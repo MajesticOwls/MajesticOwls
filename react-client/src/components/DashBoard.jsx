7a1e8051d2959dce223b4f35cf61a1e3a08339b4
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-10 18:49:02 -0700

    .

--- react-client/src/components/DashBoard.jsx
+++ react-client/src/components/DashBoard.jsx
@@ -1,98 +1,84 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const User = require('../database/index');
-const app = express();
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+import React from 'react';
+import {render} from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import FlightCard from './FlightCard.jsx';
+import FoodCard from './FoodCard.jsx';
+import SightsCard from './SightsCard.jsx';
+import WeatherCard from './WeatherCard.jsx';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import GridList from 'material-ui/GridList';
+import GoogleButton from 'react-google-button'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
 
-app.use(express.static(__dirname + '/../react-client/dist'));
-
-// Passport/Auth
-
-passport.use(new GoogleStrategy({
-    clientID: process.env.G_ID || require('./config').G_ID,
-    clientSecret: process.env.G_SECRET || require('./config').G_SECRET,
-    callbackURL: process.env.G_URL || 'http://localhost:1337/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, done) => {
-    User.findOrCreate({ googleId: profile.id }, (err, user) => {
-      return done(err, user);
-    });
-  }
-));
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
-});
-
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(session({ secret: process.env.SESSION_SECRET || require('./config').SESSION_SECRET }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../react-client/dist/index.html'));
-});
-
-
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
-
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/sign-in' }),
-  (req, res) => {
-    res.redirect('/dashboard');
-  });
-
-app.get('/dashboard', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../react-client/dist/index.html'));
-})
-
-app.get('/sign-in', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../react-client/dist/index.html'));
-})
-
-
-//FOR ADDING DATA INTO THE DATEBASE
-app.post('/database/save', (req,res) => {
-
-    const addNew = new User({
-      user: req.body.user,
-      history: req.body.history
-    })
-
-    addNew.save((err,result) => {
-      if (err) {
-        console.log('did not save');
-      } else {
-        console.log('history saved', result);
-      }
-    })
-    res.end();
-});
-
-//RETURNS LIST OF THE USERS HISTORY
-app.get('/database/return', (req,res) => {
-  User.find({user: req.body}).sort('history').exec((err,result) => {
-    if(err) {
-      console.log('Get did not return data');
-    } else {
-      res.json(result);
+class DashBoard extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
     }
-  })
+  }
+  render() {
+    const styles = {
+      titleStyle: {
+        top: 'auto',
+        right: 'auto',
+        left: 50,
+        bottom: 'auto',
+        position: 'fixed',
+      },
+      toolbarStyle: {
+        backgroundColor: '#FFF',
+      },
+      gridList: {
+        width: 'auto',
+        height: 'auto',
+        overflowY: 'auto',
+      },
+      signOutStyle: {
+        top:15,
+        right: 30,
+        left:'auto',
+        bottom: 'auto',
+        position:'fixed',
+      },
+    }
+    return(
+      <div>
+        <MuiThemeProvider>
+          <Toolbar
+            style = {styles.toolbarStyle}>
+            <ToolbarGroup firstChild={true} style={styles.titleStyle}>
+              <ToolbarTitle text="Majestic Owls" />
+            </ToolbarGroup>
+            <ToolbarGroup style={styles.signOutStyle}>
+              <Link to='/'>
+                <FlatButton
+                  label="Sign Out"
+                />
+              </Link>
+            </ToolbarGroup>
+          </Toolbar>
+        </MuiThemeProvider>
+        <MuiThemeProvider>
+          <GridList
+            cellHeight={400}
+            cols = {6}
+            style={styles.gridList}
+          >
+            <MuiThemeProvider><WeatherCard/></MuiThemeProvider>
+            <MuiThemeProvider><FlightCard/></MuiThemeProvider>
+            <MuiThemeProvider><FoodCard/></MuiThemeProvider>
+            <MuiThemeProvider><SightsCard/></MuiThemeProvider>
+          </GridList>
+        </MuiThemeProvider>
+      </div>
+    )
+  }
+}
 
-})
-
-
-const port = process.env.PORT || 1337;
-app.listen(port, () => {
-  console.log('Listening on port', port);
-});
+export default DashBoard;
