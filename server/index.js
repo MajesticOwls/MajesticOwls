06a08e0a41c0781c604df8a880933b8d33e98cfd
Jeffrey Chen <jeffreychen41@gmail.com>
2017-05-14 11:12:03 -0700

    Staging for rebase

--- server/index.js
+++ server/index.js
@@ -29,6 +29,7 @@ const checkUser = () => {
       } else {
         userIdCheck = false;
         }
+    }
   });
 }
 
@@ -174,7 +175,7 @@ app.post('/database/save', (req,res) => {
         console.log('did not save');
       } else {
         console.log('history saved', result);
-      }
+        }
     })
     res.end();
 });
